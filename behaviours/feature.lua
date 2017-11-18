340b0ebc0d1598a98804e13aa56b2eb643d9b533
Foster McLane <fkmclane@gmail.com>
2017-11-18 09:07:49 -0500

    prepare for submission

--- behaviours/feature.lua
+++ behaviours/feature.lua
@@ -2,6 +2,12 @@ function update (ticks)
 	sprite.vel_y = sprite.vel_y + ticks*80/1000
 end
 
+function observe (sprite)
+	print('observe')
+end
+
 function collide (sprite)
 	print('collision')
 end
+
+--player.observe()
