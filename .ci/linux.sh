1c89d7bcc05cc2bfb57700ce1625fbc5359a5819
Foster McLane <fkmclane@gmail.com>
2020-06-11 13:11:08 -0400

    add recursive checkout and expand linux dep install

--- .ci/linux.sh
+++ .ci/linux.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
-sudo apt install libsdl2{,-image,-mixer,-ttf,-gfx}-dev libyaml-cpp-dev
+sudo apt install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libsdl2-gfx-dev libyaml-cpp-dev
 exec pkg/linux/build.sh
