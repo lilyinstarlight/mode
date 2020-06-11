1ebf730b085f29ffdeb5fe1214b0a3606503b74f
Foster McLane <fkmclane@gmail.com>
2020-06-11 15:01:08 -0400

    add taskkill for msys2 upgrades

--- .ci/windows.ps1
+++ .ci/windows.ps1
@@ -1,7 +1,9 @@
 $env:PATH = "C:\msys64\mingw64\bin;C:\msys64\usr\bin;$env:PATH"
 
 pacman.exe -Syuu --noconfirm --noprogressbar
+taskkill /f /fi "MODULES eq msys-2.0.dll"
 pacman.exe -Syuu --noconfirm --noprogressbar
+taskkill /f /fi "MODULES eq msys-2.0.dll"
 
 pacman.exe -S --needed --noconfirm --noprogressbar base-devel zip mingw-w64-x86_64-gcc mingw-w64-x86_64-smpeg2 mingw-w64-x86_64-SDL2_ttf mingw-w64-x86_64-SDL2_mixer mingw-w64-x86_64-SDL2_image mingw-w64-x86_64-SDL2_gfx mingw-w64-x86_64-SDL2_net mingw-w64-x86_64-SDL2 mingw-w64-x86_64-yaml-cpp
 
