3c95229a85ab1ca712289f92cfa6c15c3d29299c
Foster McLane <fkmclane@gmail.com>
2020-06-12 09:45:46 -0400

    fix windows build in GH Actions (hopefully)

--- .ci/windows.ps1
+++ .ci/windows.ps1
@@ -1,10 +1,11 @@
 $env:PATH = "C:\msys64\mingw64\bin;C:\msys64\usr\bin;$env:PATH"
+$env:PKG_CONFIG_PATH = "C:\msys64\mingw64\lib\pkgconfig"
 
-pacman.exe -Syuu --noconfirm --noprogressbar
+pacman.exe -Syyuu --noconfirm --noprogressbar
 taskkill /f /fi "MODULES eq msys-2.0.dll"
-pacman.exe -Syuu --noconfirm --noprogressbar
+pacman.exe -Syyuu --noconfirm --noprogressbar
 taskkill /f /fi "MODULES eq msys-2.0.dll"
 
-pacman.exe -S --needed --noconfirm --noprogressbar base-devel zip mingw-w64-x86_64-gcc mingw-w64-x86_64-smpeg2 mingw-w64-x86_64-SDL2_ttf mingw-w64-x86_64-SDL2_mixer mingw-w64-x86_64-SDL2_image mingw-w64-x86_64-SDL2_gfx mingw-w64-x86_64-SDL2_net mingw-w64-x86_64-SDL2 mingw-w64-x86_64-yaml-cpp
+bash.exe -c "pacman.exe -S --needed --noconfirm --noprogressbar base-devel zip mingw-w64-x86_64-gcc mingw-w64-x86_64-smpeg2 mingw-w64-x86_64-SDL2_ttf mingw-w64-x86_64-SDL2_mixer mingw-w64-x86_64-SDL2_image mingw-w64-x86_64-SDL2_gfx mingw-w64-x86_64-SDL2_net mingw-w64-x86_64-SDL2 mingw-w64-x86_64-yaml-cpp 2>&1"
 
 bash.exe pkg/windows/build.sh
