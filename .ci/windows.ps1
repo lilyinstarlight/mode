d4d98798bc741c35725222efff333a10a9538c10
Foster McLane <fkmclane@gmail.com>
2020-06-20 14:52:28 -0400

    fix misc github actions group formatting issues

--- .ci/windows.ps1
+++ .ci/windows.ps1
@@ -16,10 +16,10 @@ $env:PKG_CONFIG_PATH = "C:\msys64\mingw64\lib\pkgconfig"
 
 Start-Group -GroupMessage 'Update MSYS2'
 
-Write-Output -InputObject 'pacman.exe -Syyuu --noconfirm --noprogressbar'
+Write-Output -InputObject '+ pacman.exe -Syyuu --noconfirm --noprogressbar'
 pacman.exe -Syyuu --noconfirm --noprogressbar
 taskkill /f /fi 'MODULES eq msys-2.0.dll'
-Write-Output -InputObject 'pacman.exe -Syyuu --noconfirm --noprogressbar'
+Write-Output -InputObject '+ pacman.exe -Syyuu --noconfirm --noprogressbar'
 pacman.exe -Syyuu --noconfirm --noprogressbar
 taskkill /f /fi 'MODULES eq msys-2.0.dll'
 
@@ -27,7 +27,7 @@ End-Group
 
 Start-Group -GroupMessage 'Install dependencies'
 
-Write-Output -InputObject "bash.exe -c 'exec pacman.exe -S --needed --noconfirm --noprogressbar base-devel zip mingw-w64-x86_64-gcc mingw-w64-x86_64-imagemagick mingw-w64-x86_64-smpeg2 mingw-w64-x86_64-SDL2_ttf mingw-w64-x86_64-SDL2_mixer mingw-w64-x86_64-SDL2_image mingw-w64-x86_64-SDL2_gfx mingw-w64-x86_64-SDL2_net mingw-w64-x86_64-SDL2 mingw-w64-x86_64-yaml-cpp 2>&1'"
+Write-Output -InputObject "+ bash.exe -c 'exec pacman.exe -S --needed --noconfirm --noprogressbar base-devel zip mingw-w64-x86_64-gcc mingw-w64-x86_64-imagemagick mingw-w64-x86_64-smpeg2 mingw-w64-x86_64-SDL2_ttf mingw-w64-x86_64-SDL2_mixer mingw-w64-x86_64-SDL2_image mingw-w64-x86_64-SDL2_gfx mingw-w64-x86_64-SDL2_net mingw-w64-x86_64-SDL2 mingw-w64-x86_64-yaml-cpp 2>&1'"
 bash.exe -c 'exec pacman.exe -S --needed --noconfirm --noprogressbar base-devel zip mingw-w64-x86_64-gcc mingw-w64-x86_64-imagemagick mingw-w64-x86_64-smpeg2 mingw-w64-x86_64-SDL2_ttf mingw-w64-x86_64-SDL2_mixer mingw-w64-x86_64-SDL2_image mingw-w64-x86_64-SDL2_gfx mingw-w64-x86_64-SDL2_net mingw-w64-x86_64-SDL2 mingw-w64-x86_64-yaml-cpp 2>&1'
 
 End-Group
--- pkg/windows/build.sh
+++ pkg/windows/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 start_group() {
   if [ "$GITHUB_ACTIONS" == "true" ]; then
-    echo "::group::$(echo -e "$*" | sed -e '1h;2,$H;$!d;g' -e 's/%/%25/g' -e 's/\r/%0D/g' -e 's/\n/%0A/g')"
+    echo "::group::$(echo -e "$*" | sed -e '1h;2,$H;$!d;g' -e 's/%/%25/g' -e 's/\r/%0D/g' -e 's/\n/%0A/g')" >&2
   fi
 }
 
 end_group() {
   if [ "$GITHUB_ACTIONS" == "true" ]; then
-    echo "::endgroup::"
+    echo "::endgroup::" >&2
   fi
 }
 
