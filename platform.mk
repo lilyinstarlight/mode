ebf75a3736f0b946f0f565bdc56e2a0c8ede909f
Foster McLane <fkmclane@gmail.com>
2020-06-27 09:37:25 -0400

    just use -I instead of -isystem because some platforms inject system paths with -I and shadows our vendored includes

--- platform.mk
+++ platform.mk
@@ -12,7 +12,7 @@ CXX=g++
 
 ifeq ($(DEBUG),1)
 
-CXXFLAGS=-std=c++17 -fsanitize=address -ggdb -Wall -Wpedantic -Wextra -Weffc++ -Werror -isystem $(VENDORDIR) -isystem $(VENDORDIR)/yaml-cpp/include -isystem $(VENDORDIR)/lua -isystem $(VENDORDIR)/sol3/single/include `sdl2-config --cflags`
+CXXFLAGS=-std=c++17 -fsanitize=address -ggdb -Wall -Wpedantic -Wextra -Weffc++ -Werror -I $(VENDORDIR) -I $(VENDORDIR)/yaml-cpp/include -I $(VENDORDIR)/lua -I $(VENDORDIR)/sol3/single/include `sdl2-config --cflags`
 LDFLAGS=-std=c++17 -fsanitize=address
 LDLIBS=-lm -ldl `sdl2-config --libs` -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lSDL2_gfx
 
@@ -24,7 +24,7 @@ LUA_LDLIBS=-ldl -lreadline -lhistory -lncurses
 
 else
 
-CXXFLAGS=-std=c++17 -O2 -DNDEBUG -Wall -Wpedantic -Wextra -Weffc++ -isystem $(VENDORDIR) -isystem $(VENDORDIR)/yaml-cpp/include -isystem $(VENDORDIR)/lua -isystem $(VENDORDIR)/sol3/single/include `sdl2-config --cflags`
+CXXFLAGS=-std=c++17 -O2 -DNDEBUG -Wall -Wpedantic -Wextra -Weffc++ -I $(VENDORDIR) -I $(VENDORDIR)/yaml-cpp/include -I $(VENDORDIR)/lua -I $(VENDORDIR)/sol3/single/include `sdl2-config --cflags`
 LDFLAGS=-std=c++17 -O2
 LDLIBS=-lm -ldl `sdl2-config --libs` -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lSDL2_gfx
 
@@ -48,7 +48,7 @@ CXX=clang++
 
 ifeq ($(DEBUG),1)
 
-CXXFLAGS=-std=c++17 -fsanitize=address,undefined -ggdb -Wall -Wpedantic -Wextra -Weffc++ -Werror -isystem $(VENDORDIR) -isystem $(VENDORDIR)/yaml-cpp/include -isystem $(VENDORDIR)/lua -isystem $(VENDORDIR)/sol3/single/include `sdl2-config --cflags`
+CXXFLAGS=-std=c++17 -fsanitize=address,undefined -ggdb -Wall -Wpedantic -Wextra -Weffc++ -Werror -I $(VENDORDIR) -I $(VENDORDIR)/yaml-cpp/include -I $(VENDORDIR)/lua -I $(VENDORDIR)/sol3/single/include `sdl2-config --cflags`
 LDFLAGS=-std=c++17 -fsanitize=address,undefined
 LDLIBS=-lm -ldl `sdl2-config --libs` -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lSDL2_gfx
 
@@ -60,7 +60,7 @@ LUA_LDLIBS=-ldl -lreadline -lncurses
 
 else
 
-CXXFLAGS=-std=c++17 -O2 -DNDEBUG -Wall -Wpedantic -Wextra -Weffc++ -isystem $(VENDORDIR) -isystem $(VENDORDIR)/yaml-cpp/include -isystem $(VENDORDIR)/lua -isystem $(VENDORDIR)/sol3/single/include `sdl2-config --cflags`
+CXXFLAGS=-std=c++17 -O2 -DNDEBUG -Wall -Wpedantic -Wextra -Weffc++ -I $(VENDORDIR) -I $(VENDORDIR)/yaml-cpp/include -I $(VENDORDIR)/lua -I $(VENDORDIR)/sol3/single/include `sdl2-config --cflags`
 LDFLAGS=-std=c++17 -O2
 LDLIBS=-lm -ldl `sdl2-config --libs` -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lSDL2_gfx
 
@@ -84,7 +84,7 @@ CXX=g++
 
 ifeq ($(DEBUG),1)
 
-CXXFLAGS=-std=c++17 -mwindows -ggdb -Wall -Wpedantic -Wextra -Weffc++ -Werror -isystem $(VENDORDIR) -isystem $(VENDORDIR)/yaml-cpp/include -isystem $(VENDORDIR)/lua -isystem $(VENDORDIR)/sol3/single/include `sdl2-config --cflags`
+CXXFLAGS=-std=c++17 -mwindows -ggdb -Wall -Wpedantic -Wextra -Weffc++ -Werror -I $(VENDORDIR) -I $(VENDORDIR)/yaml-cpp/include -I $(VENDORDIR)/lua -I $(VENDORDIR)/sol3/single/include `sdl2-config --cflags`
 LDFLAGS=-std=c++17 -mwindows
 LDLIBS=-lm `sdl2-config --libs` -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lSDL2_gfx
 
@@ -96,7 +96,7 @@ LUA_LDLIBS=-lreadline -lhistory -lncurses
 
 else
 
-CXXFLAGS=-std=c++17 -O2 -mwindows -DNDEBUG -Wall -Wpedantic -Wextra -Weffc++ -isystem $(VENDORDIR) -isystem $(VENDORDIR)/yaml-cpp/include -isystem $(VENDORDIR)/lua -isystem $(VENDORDIR)/sol3/single/include `sdl2-config --cflags`
+CXXFLAGS=-std=c++17 -O2 -mwindows -DNDEBUG -Wall -Wpedantic -Wextra -Weffc++ -I $(VENDORDIR) -I $(VENDORDIR)/yaml-cpp/include -I $(VENDORDIR)/lua -I $(VENDORDIR)/sol3/single/include `sdl2-config --cflags`
 LDFLAGS=-std=c++17 -O2 -mwindows
 LDLIBS=-lm `sdl2-config --libs` -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lSDL2_gfx
 
@@ -120,7 +120,7 @@ CXX=c++
 
 ifeq ($(DEBUG),1)
 
-CXXFLAGS=-std=c++17 -g -Wall -Wpedantic -Wextra -Weffc++ -Werror -isystem $(VENDORDIR) -isystem $(VENDORDIR)/yaml-cpp/include -isystem $(VENDORDIR)/lua -isystem $(VENDORDIR)/sol3/single/include `sdl2-config --cflags`
+CXXFLAGS=-std=c++17 -g -Wall -Wpedantic -Wextra -Weffc++ -Werror -I $(VENDORDIR) -I $(VENDORDIR)/yaml-cpp/include -I $(VENDORDIR)/lua -I $(VENDORDIR)/sol3/single/include `sdl2-config --cflags`
 LDFLAGS=-std=c++17
 LDLIBS=-lm -ldl `sdl2-config --libs` -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lSDL2_gfx
 
@@ -132,7 +132,7 @@ LUA_LDLIBS=-ldl -lreadline -lhistory -lncurses
 
 else
 
-CXXFLAGS=-std=c++17 -DNDEBUG -Wall -Wpedantic -Wextra -Weffc++ -isystem $(VENDORDIR) -isystem $(VENDORDIR)/yaml-cpp/include -isystem $(VENDORDIR)/lua -isystem $(VENDORDIR)/sol3/single/include `sdl2-config --cflags`
+CXXFLAGS=-std=c++17 -DNDEBUG -Wall -Wpedantic -Wextra -Weffc++ -I $(VENDORDIR) -I $(VENDORDIR)/yaml-cpp/include -I $(VENDORDIR)/lua -I $(VENDORDIR)/sol3/single/include `sdl2-config --cflags`
 LDFLAGS=-std=c++17
 LDLIBS=-lm -ldl `sdl2-config --libs` -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lSDL2_gfx
 
